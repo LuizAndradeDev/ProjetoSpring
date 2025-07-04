36ae9b781db239dc1f46673752639ee6ec4ef845
Luiz Souza <luizsouzandrade.dev@gmail.com>
2025-07-04 11:28:18 -0300

    primeiro commit do projeto

---
+++ src/main/java/br/com/luizsa/ScreenS/ScreenSApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizsa.ScreenS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luizsa/ScreenS/ScreenSApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizsa.ScreenS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
