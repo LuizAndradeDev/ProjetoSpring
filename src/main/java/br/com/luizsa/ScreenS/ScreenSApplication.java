7cf12ad520b257099e2e9eb83f606bda045b0a92
Luiz Souza <luizsouzandrade.dev@gmail.com>
2025-07-04 11:30:41 -0300

    Transformando em uma aplicação de terminal

--- src/main/java/br/com/luizsa/ScreenS/ScreenSApplication.java
+++ src/main/java/br/com/luizsa/ScreenS/ScreenSApplication.java
@@ -1,13 +1,18 @@
 package br.com.luizsa.ScreenS;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenSApplication {
+public class ScreenSApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenSApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Projeto Spring teste");
+	}
 }
