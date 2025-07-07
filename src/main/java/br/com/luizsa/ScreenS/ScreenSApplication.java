16ab39ec31ce2adf37e188c08782345248d2d850
Luiz Souza <luizsouzandrade.dev@gmail.com>
2025-07-07 11:28:49 -0300

    Nova consulta de temporada

--- src/main/java/br/com/luizsa/ScreenS/ScreenSApplication.java
+++ src/main/java/br/com/luizsa/ScreenS/ScreenSApplication.java
@@ -11,28 +11,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScreenSApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenSApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenSApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception{
-		var consumo = new ConsumoAPI();
-		var json = consumo.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+    @Override
+    public void run(String... args) throws Exception {
+        var consumo = new ConsumoAPI();
+        var json = consumo.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
 //		var json2 = consumo.obterDados("https://coffee.alexflipnote.dev/random.json");
 //		System.out.println(json2);
-		System.out.println(json);
+        System.out.println(json);
 
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        ConverteDados conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 
-		var jsonEpisodio = consumo.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
-		DadosEpisodio  dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+        var jsonEpisodio = consumo.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
+        DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
 
-		System.out.println(dados);
+        System.out.println(dados);
 
-		System.out.println(dadosEpisodio);
+        System.out.println(dadosEpisodio);
 
-	}
+    }
 
 }
