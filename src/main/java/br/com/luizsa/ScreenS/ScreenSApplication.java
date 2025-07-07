bbfc4b9215cdfc7ed2aee7847b6bd749f7b9f5e5
Luiz Souza <luizsouzandrade.dev@gmail.com>
2025-07-07 10:48:27 -0300

    Adicionando uma forma pra ler dados de episodios

--- src/main/java/br/com/luizsa/ScreenS/ScreenSApplication.java
+++ src/main/java/br/com/luizsa/ScreenS/ScreenSApplication.java
@@ -1,5 +1,6 @@
 package br.com.luizsa.ScreenS;
 
+import br.com.luizsa.ScreenS.model.DadosEpisodio;
 import br.com.luizsa.ScreenS.model.DadosSerie;
 import br.com.luizsa.ScreenS.service.ConsumoAPI;
 import br.com.luizsa.ScreenS.service.ConverteDados;
@@ -24,7 +25,13 @@ public class ScreenSApplication implements CommandLineRunner {
 
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+
+		var jsonEpisodio = consumo.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
+		DadosEpisodio  dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+
 		System.out.println(dados);
+
+		System.out.println(dadosEpisodio);
 
 	}
 
